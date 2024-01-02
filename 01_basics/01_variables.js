f21214f34664a4f00a657a99c932f7d3586d7120
sachinpandey20 <86186664+sachinpandey20@users.noreply.github.com>
2024-01-02 22:02:58 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 3242
+let accountEmail = "sachinpandey@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur" 
+
+//accountId = 2 // not allowed
+
+accountEmail = "pandeysachin@gmail.com"
+accountPassword = "34242"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
